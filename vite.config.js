c80d46c42160d5043f33d811b17270d8bc76dafc
macangel23 <macangel2303@gmail.com>
2023-01-25 20:37:22 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/pageMenu.css',
+                'resources/js/menu.js'
             ],
             refresh: true,
         }),
