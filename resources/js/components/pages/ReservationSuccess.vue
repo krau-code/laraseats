7006ac37c42759e2cab0ce0cf12263987caf7cfa
Paulo Amielle Suratos <paulo.suratos@gmail.com>
2023-02-11 20:36:44 +0800

    update reservation success message

--- resources/js/components/pages/ReservationSuccess.vue
+++ resources/js/components/pages/ReservationSuccess.vue
@@ -1,12 +1,12 @@
 <template>
     <title>Reserve Successful | LaraSeats</title>
 
-    <div class="mx-auto md:w-3/5 text-white text-4xl mt-6 leading-relaxed">
+    <div class="mx-auto mt-6 text-4xl leading-relaxed text-white md:w-3/5">
         <p>Hi, <span class="font-bold capitalize">{{ $route.params.name }}!</span></p>
-        <p class="mt-2">You have successfully reserved <span class="font-bold">{{ $route.params.quantity }}</span> seat/s with a total amount of <span class="font-bold">&#8369;{{ $route.params.total_amount }}.00</span>.</p>
+        <p class="mt-2">You have successfully reserved <span class="font-bold">{{ $route.params.quantity }}</span> seat/s with a total amount of <span class="font-bold">&#8369;{{ $route.params.total_amount }}</span>.</p>
         <p class="mt-2">See you there!</p>
 
-        <a href="/" class="hover:text-gray-400 text-xl uppercase text-gray-300 underline">Go back to homepage</a>
+        <a href="/" class="text-xl text-gray-300 underline uppercase hover:text-gray-400">Go back to homepage</a>
     </div>
 </template>
 
